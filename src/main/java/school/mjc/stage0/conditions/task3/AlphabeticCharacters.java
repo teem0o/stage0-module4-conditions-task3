20b181645d3640ffafdfec1d8c9875cedee86faa
temo <chxaidzetemo@gmail.com>
2022-07-10 20:42:54 +0400

    tgg

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (character=='a'||character=='e'||character=='o'||character=='i'||character=='u'){
+            System.out.println("Vowel");
+        }else if (character >= 'a'&& character <= 'z'){
+            System.out.println("Consonant");
+        }else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary < 0) {
+            System.out.println("wrong input!");
+        } else if (salary<=10000){
+            System.out.println(salary * 0.85);
+        }
+        else if (salary <= 20000 && salary > 10000) {
+            System.out.println(salary * 0.82);
+        }
+        else if (salary > 20000 ) {
+            System.out.println(salary * 0.8);
+        }
+
+
     }
 }
